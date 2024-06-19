14a866c1a8ee0c8e4a652b7a0b8b585258c35fad
maslov <a.maslov@worksolutions.ru>
2024-06-19 13:59:10 +0300

    Added validating at booking request

--- api/src/Request/BookingRequest.php
+++ api/src/Request/BookingRequest.php
@@ -11,12 +11,36 @@ class BookingRequest
         #[Assert\NotNull]
         public int $price,
         #[Assert\NotNull]
+        #[Assert\Type("\DateTimeInterface")]
         public DateTime $startDate,
         #[Assert\NotNull]
+        #[Assert\Type("\DateTimeInterface")]
         public DateTime $birthDate,
-        #[Assert\NotNull]
-        public DateTime $paymentDate
+        #[Assert\Type("\DateTimeInterface")]
+        public ?DateTime $paymentDate
     ) {
+    }
+
+    #[Assert\IsTrue(null, "Birth date must be earlier than now date")]
+    public function isBirthDate(): bool
+    {
+        return $this->birthDate <= new DateTime();
+    }
+
+    #[Assert\IsTrue(null, "Now date must be earlier than start date")]
+    public function isStartDate(): bool
+    {
+        return $this->startDate >= new DateTime();
+    }
+
+    #[Assert\IsTrue(null, "Now date must be earlier than payment date")]
+    public function isPaymentDate(): bool
+    {
+        if ($this->paymentDate === null) {
+            return true;
+        }
+
+        return $this->paymentDate >= new DateTime();
     }
 
     public function getPrice(): int
@@ -24,7 +48,7 @@ class BookingRequest
         return $this->price;
     }
 
-    public function getPaymentDate(): DateTime
+    public function getPaymentDate(): ?DateTime
     {
         return $this->paymentDate;
     }
