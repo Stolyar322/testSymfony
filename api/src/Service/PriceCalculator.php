85883f63fdc59c11442dbf32c90da579c3777e08
maslov <a.maslov@worksolutions.ru>
2024-06-19 13:32:54 +0300

    Created price calculator

--- api/src/Service/PriceCalculator.php
+++ api/src/Service/PriceCalculator.php
@@ -4,13 +4,24 @@ namespace App\Service;
 
 class PriceCalculator
 {
+    private DiscountCalculator $discountCalculator;
+    public function __construct(DiscountCalculator $discountCalculator)
+    {
+        $this->discountCalculator = $discountCalculator;
+    }
+
     public function calculate(
         int $price,
         \DateTime $birthDate,
         \DateTime $startDate,
         \DateTime $paymentDate
     ): int {
+        $discount = $this->discountCalculator->calculateDiscount(
+            $startDate,
+            $paymentDate,
+            $birthDate
+        );
 
-        return $price;
+        return $price * (1 - $discount);
     }
 }
